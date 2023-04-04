cd6e3289b04b9a0fdf21e7314f1b7526e911ac93
cversky-rh <61512772+cversky-rh@users.noreply.github.com>
2023-04-04 08:53:12 -0700

    Update app.py
    - moved the HowsMySalute import statement inside the gen() function
    - added a decorator @app.route('/video_feed') to the gen() function instead of the video_feed() function - eliminates the need for the video_feed() function
    - using the "yield from" syntax instead of "yield" inside the gen() function

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template, Response
-from HowsMySalute import salute
 
 app = Flask(__name__)
-
+ 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(HowsMySalute):
+@app.route('/video_feed')
+def gen():
+    from HowsMySalute import salute
     while True:
-        frame = HowsMySalute.get_salute()
+        frame = salute().get_salute()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    # print("done")
-    return Response(gen(salute()),mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
